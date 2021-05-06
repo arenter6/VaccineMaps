fdda45161bf6e19c703f9d8bafc9b5da6a2a2515
= <dapang@ucsc.edu>
2021-05-06 15:21:18 -0700

    Alpha v1.1 Added input range and clear map

--- static/js/test_map.js
+++ static/js/test_map.js
@@ -1,13 +1,19 @@
 //TODO!!
 //Implement search by location
-//Show the nearest locations (default 25 miles)
+//Show the nearest locations (default 25 miles) Leaflet uses meters by default so must multiply input by 1600!
 //Have side table sorted by nearest location and availability (Other options include range and availability)
 //Selection Menu
 //Sort by availability and closest to user input
 
+//Dynamic map scrolling that updates the pins and table near map center
+
 //Corner cases!!
-    //Make sure user cannot enter in non-US zipcode or non-US adddress
+    //Make sure user cannot enter in non-US zipcode or non-US address
     //Make sure user only inputs alpha-numerical input
+
+    //Make sure range is non negative and zipcode is between 500 and 99500
+    //Clear markers every time a new zip code is entered
+    //Bug where if the state is small, will only display that state's locations  <-- Import entire US instead of state, problem -> may cost more time to run
 
 //Change some jQuery to Axios
     //XML parser
@@ -29,8 +35,9 @@ let init = (app) => {
         search_location: "",
         state: "CA", //by Default it is california
         zipcode: "",
-        range: 25,
-
+        geoJson: null,
+        map_layer: null,
+        range: 10,
         //Vaccine types (not likely)
     };
 
@@ -38,11 +45,20 @@ let init = (app) => {
         app.vue.zipcode = "";
     };
 
+
+
+
+
     app.search = function() {
         let request_city = "https://secure.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&XML=<CityStateLookupRequest USERID=\"";
         request_city += user_id + "\"><ZipCode ID='0'><Zip5>";
-        request_city += app.vue.zipcode;
-        request_city += "</Zip5></ZipCode></CityStateLookupRequest>";
+        request_city += app.vue.zipcode + "</Zip5></ZipCode></CityStateLookupRequest>";
+
+        if (app.vue.geoJson) {
+            console.log("Removed the map layer");
+            app.vue.geoJson.clearLayers();
+        }
+
         axios.get(request_city).then(function(response) {
             console.log(response);
             var xml = $.parseXML(response.data),
@@ -52,30 +68,41 @@ let init = (app) => {
             console.log(app.vue.state);
             $.get("https://nominatim.openstreetmap.org/search?format=json&limit=3&q="+app.vue.zipcode+"+"+app.vue.state, function(coord){
                 console.log(coord);
-                let lat = coord[0]['lat'];
-                let lng = coord[0]['lon'];
+                var lat = coord[0]['lat'];
+                var lng = coord[0]['lon'];
                 map.flyTo([lat, lng], 13);
+
+                //var lng_float = Math.abs(parseFloat(lng), map.getBounds().getEast());
+                //console.log(lng_float);
+                var distance = app.vue.range*1600; //Converts miles from range input to meters
+                console.log("The radius is " + distance + " meters");
                 axios.get("https://www.vaccinespotter.org/api/v0/states/" + app.vue.state + ".json").then(function(response) {
                     let data = response.data;
                     console.log("Successfully loaded data");
-                    console.log(data)
-                    L.geoJson(data, { onEachFeature:
-                          function(feature,layer) {
-                              var popup = "<b>" + feature.properties.name + "</b>";
-                              popup += "<h2>" + "Provider: " + feature.properties.provider_brand_name + "</h2>";
-                              popup += "<h2>" + "Address: " + feature.properties.address + "</h2>";
-                              popup += "<h2>" + "City: " + feature.properties.city + "</h2>";
-                              popup += "<h2>" + "Zip Code: " + feature.properties.postal_code + "</h2>";
-                              //Calculate distance from user input
-                              //No way to tell what vaccine each site carries
-                              if(feature.properties.appointments_available == true) {
-                                    popup += "<h2 class='has-text-success'>Available</h2>";
-                                    popup += "<a href='" + feature.properties.url + "'>" + "Book an Appointment" + "</a>";
-                              } else {
-                                    popup += "<h2 class='has-text-danger'>Not Available</h2>";
-                              }
-                              layer.bindPopup(popup);
-                          }
+                    console.log(data);
+                    app.vue.geoJson = L.geoJson(data, { filter:
+                        function(feature, layer) {
+                            if(L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]).distanceTo(L.latLng([lat,lng])) <= distance) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+                    }).bindPopup(function (layer) {
+                        var popup = "<b>" + layer.feature.properties.name + "</b>";
+                        popup += "<h2>" + "Provider: " + layer.feature.properties.provider_brand_name + "</h2>";
+                        popup += "<h2>" + "Address: " + layer.feature.properties.address + "</h2>";
+                        popup += "<h2>" + "City: " + layer.feature.properties.city + "</h2>";
+                        popup += "<h2>" + "Zip Code: " + layer.feature.properties.postal_code + "</h2>";
+                        //Calculate distance from user input
+                        //No way to tell what vaccine each site carries
+                        if(layer.feature.properties.appointments_available == true) {
+                            popup += "<h2 class='has-text-success'>Available</h2>";
+                            popup += "<a href='" + layer.feature.properties.url + "'>" + "Book an Appointment" + "</a>";
+                        } else {
+                            popup += "<h2 class='has-text-danger'>Not Available</h2>";
+                        }
+                        return popup;
                     }).addTo(map);
                     app.clear_search();
                 });
@@ -102,6 +129,7 @@ let init = (app) => {
 init(app);
 
 var map = L.map('mapid').setView([36.9881, -122.0582], 13);
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Room 12 Prototype Mark II Alpha 0.91 Beta 4.20 Demo 6 Area 51 Operation Made You Look',
