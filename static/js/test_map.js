f23c4b55edd1ba0314332992826a49a8b7311019
arenter6 <arenter6@surface.com>
2021-05-09 17:14:32 -0700

    test_map: Improved structure & added comments

--- static/js/test_map.js
+++ static/js/test_map.js
@@ -1,6 +1,6 @@
 //TODO!!
 //Implement search by location
-//Show the nearest locations (default 25 miles) Leaflet uses meters by default so must multiply input by 1600!
+//Show the nearest locations (default 10 miles) Leaflet uses meters by default so must multiply input by 1600!
 //Have side table sorted by nearest location and availability (Other options include range and availability)
 //Selection Menu
 //Sort by availability and closest to user input
@@ -37,7 +37,6 @@ let init = (app) => {
         zipcode: "",
         geoJson: null,
         map_layer: null,
-        //range: 10,
         range: { // Default search radius is 10 miles
             number: 10,
         },
@@ -49,27 +48,37 @@ let init = (app) => {
     };
 
     app.search = function() {
-        
+        // USPS Adddress API - POST request - City/State Lookup Web Tool
+        // Params.: API: CityStateLookup,
+        // XML: <CityStateLookupRequest USERID="{{USERID}}"><ZipCode ID="0"><Zip5>20024</Zip5></ZipCode></CityStateLookupRequest>
         let request_city = "https://secure.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&XML=<CityStateLookupRequest USERID=\"";
+        // Concatenate USERID to request
         request_city += user_id + "\"><ZipCode ID='0'><Zip5>";
+        // Add zipcode from input and finish request string
         request_city += app.vue.zipcode + "</Zip5></ZipCode></CityStateLookupRequest>";
 
         if (app.vue.geoJson) {
             console.log("Removed the map layer");
             app.vue.geoJson.clearLayers();
         }
-
+        // Parse USPS City/State Lookup request
         axios.get(request_city).then(function(response) {
             console.log(response);
             var xml = $.parseXML(response.data),
                       $xml = $( xml ),
                       $state = $xml.find('State');
+            // Assign U.S. state variable
             app.vue.state = $state.text();
             console.log(app.vue.state);
+            // Process GET request from openstreetmap.org using Zip code and U.S. state
             $.get("https://nominatim.openstreetmap.org/search?format=json&limit=3&q="+app.vue.zipcode+"+"+app.vue.state, function(coord){
                 console.log(coord);
+                // request will fill latitude and longtitude variables
                 var lat = coord[0]['lat'];
                 var lng = coord[0]['lon'];
+                // Use flyTo() to smoothly interpolate between locations
+                // Params.: center: [lat, lng] - where lat, lng are numbers; centers map position
+                // zoom: number - zoom level set to 13 when searches are conducted
                 map.flyTo([lat, lng], 13);
 
                 //var lng_float = Math.abs(parseFloat(lng), map.getBounds().getEast());
@@ -128,15 +137,34 @@ let init = (app) => {
 
 init(app);
 
+// setView(); position map, to UCSC Campus w/ zoom level 13 when Map initiates
 var map = L.map('mapid').setView([36.9881, -122.0582], 13);
 
-
+// Mapbox Static Tiles API
+// https://docs.mapbox.com/help/troubleshooting/migrate-legacy-static-tiles-api/
+// https://docs.mapbox.com/api/maps/static-tiles/
+// Tiles are 512x512 pixels and are offset by 1 zoom level.
+// {z}/{x}/{y}: Are the tile coordinates. They specify the tile's zoom level, column, and row respectively.
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    // String to be shown in the attribution control. Legal obligation towards copyright holders and tile providers 
     attribution: 'Room 12 Prototype Mark II Alpha 0.91 Beta 4.20 Demo 6 Area 51 Operation Made You Look',
+    
+    // The max zoom level up to which this layer will be displayed (inclusive)
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    // ***** WARNING: If you do not include the tileSize: 512 and zoomOffset: -1 options, your map will still
+    // ***** load but labels, icons, and other features may appear much smaller than expected.  
+    
+    // Width and height of tiles in the grid. Default is 512
+    // Use a num if width and height are equal, or L.point(width, height) otherwise
     tileSize: 512,
+    
+    // The zoom number used in tile URLs will be offset with this value. Default is 0
     zoomOffset: -1,
+    
+    // The ID of the style from which to return a raster tile     
+    id: 'mapbox/streets-v11',
+
+    // Maps API key
     accessToken: api_key
 }).addTo(map);
 
