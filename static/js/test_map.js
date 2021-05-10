6907323fac77a6f8ff9b4b4caf0f16c1ca7a9ed2
= <dapang@ucsc.edu>
2021-05-10 02:59:20 -0700

    Test of adding slider bar input

--- static/js/test_map.js
+++ static/js/test_map.js
@@ -1,28 +1,33 @@
-//TODO!!
+//TODO Search and Side Menu
 //Implement search by location
-//Show the nearest locations (default 25 miles) Leaflet uses meters by default so must multiply input by 1600!
-//Have side table sorted by nearest location and availability (Other options include range and availability)
-//Selection Menu
-//Sort by availability and closest to user input
+//Selection Menu (Range, vaccine type, user reviewed, highest rated)
+    //Sort by availability and closest to user input
+    //Have side table sorted by nearest location and availability (Other options include range and availability)
 
-//Dynamic map scrolling that updates the pins and table near map center
+//TODO MAP
+    //Dynamic map scrolling that updates the pins and table near map center
 
-//Corner cases!!
+
+//TODO Corner cases!!
     //Make sure user cannot enter in non-US zipcode or non-US address
-    //Make sure user only inputs alpha-numerical input
-
+        //check using API and validate
+    //Make sure user only inputs alpha-numerical input (non-negative)
+        //Parse string
     //Make sure range is non negative and zipcode is between 500 and 99500
-    //Clear markers every time a new zip code is entered
+        //Make either an v-if statement or form validator
     //Bug where if the state is small, will only display that state's locations  <-- Import entire US instead of state, problem -> may cost more time to run
 
+    //Check to see if range is valid (0-100) also add slider (css sass)
+
+//TODO code cleanup
 //Change some jQuery to Axios
     //XML parser
 
-//TODO
+//TODO customization
   //Each pin should have a custom icon
   //Green pin for available, Red pin for not available
   //If a site has been reviewed, blue stroke but with availability colored pin
-  //If pin is clicked on, call database to get table of reviews
+  //If pin is clicked on, call database to get average reviews and table of reviews
   //Implement table that has coordinates from its address
   //Include images (if have time)
 
@@ -32,12 +37,13 @@ let app = {};
 
 let init = (app) => {
     app.data = {
-        search_location: "",
+        search_location: null,
         state: "CA", //by Default it is california
-        zipcode: "",
+        zipcode: null,
         geoJson: null,
-        map_layer: null,
         range: 10,
+        zipcode_error: false,
+        range_error: false,
         //Vaccine types (not likely)
     };
 
@@ -45,7 +51,24 @@ let init = (app) => {
         app.vue.zipcode = "";
     };
 
+    //TODO
+    app.check_form = function (e) {
+      if (!app.vue.zipcode) {
+        app.vue.zipcode_error = true;
+      }
+      //TODO check validity of zipcode using api/csv
+      if (!this.email) {
+        this.errors.push('Email required.');
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push('Valid email required.');
+      }
 
+      if (!app.vue.zipcode) {
+        app.vue.zipcode_error = true;
+      }
+
+      e.preventDefault();
+    };
 
 
 
@@ -72,11 +95,11 @@ let init = (app) => {
                 var lng = coord[0]['lon'];
                 map.flyTo([lat, lng], 13);
 
-                //var lng_float = Math.abs(parseFloat(lng), map.getBounds().getEast());
-                //console.log(lng_float);
                 var distance = app.vue.range*1600; //Converts miles from range input to meters
                 console.log("The radius is " + distance + " meters");
-                axios.get("https://www.vaccinespotter.org/api/v0/states/" + app.vue.state + ".json").then(function(response) {
+                //may want to use USA instead of state in case of zip codes on edge
+                //axios.get("https://www.vaccinespotter.org/api/v0/states/" + app.vue.state + ".json").then(function(response) {
+                axios.get("https://www.vaccinespotter.org/api/v0/US.json").then(function(response) {
                     let data = response.data;
                     console.log("Successfully loaded data");
                     console.log(data);
@@ -89,16 +112,16 @@ let init = (app) => {
                             }
                         }
                     }).bindPopup(function (layer) {
-                        var popup = "<b>" + layer.feature.properties.name + "</b>";
-                        popup += "<h2>" + "Provider: " + layer.feature.properties.provider_brand_name + "</h2>";
-                        popup += "<h2>" + "Address: " + layer.feature.properties.address + "</h2>";
-                        popup += "<h2>" + "City: " + layer.feature.properties.city + "</h2>";
-                        popup += "<h2>" + "Zip Code: " + layer.feature.properties.postal_code + "</h2>";
+                        var popup = "<b>" + layer.feature.properties.name + "</b>"
+                               + "<h2>" + "Provider: " + layer.feature.properties.provider_brand_name + "</h2>"
+                               + "<h2>" + "Address: " + layer.feature.properties.address + "</h2>"
+                               + "<h2>" + "City: " + layer.feature.properties.city + "</h2>"
+                               + "<h2>" + "Zip Code: " + layer.feature.properties.postal_code + "</h2>";
                         //Calculate distance from user input
                         //No way to tell what vaccine each site carries
                         if(layer.feature.properties.appointments_available == true) {
-                            popup += "<h2 class='has-text-success'>Available</h2>";
-                            popup += "<a href='" + layer.feature.properties.url + "'>" + "Book an Appointment" + "</a>";
+                            popup += "<h2 class='has-text-success'>Available</h2>"
+                                  + "<a target='_blank' href='" + layer.feature.properties.url + "'>" + "Book an Appointment" + "</a>";
                         } else {
                             popup += "<h2 class='has-text-danger'>Not Available</h2>";
                         }
@@ -113,6 +136,7 @@ let init = (app) => {
     app.methods = {
         clear_search: app.clear_search,
         search: app.search,
+        check_form: app.check_form,
     };
 
     app.vue = new Vue({
