d2a9952b2e80cb9450fdf6f39943de115afc67d9
Colin Xie <cxxie@ucsc.edu>
2021-06-03 12:29:32 -0700

    Added about us

--- controllers.py
+++ controllers.py
@@ -323,3 +323,9 @@ def test_data():
 def get_data():
     data_url = request.params.get('data_url')
     return dict(data_url=data_url)
+
+#About Us Redirect
+@action('about_us')
+@action.uses(db, session, auth, 'about_us.html')
+def about_us():
+    return dict()
