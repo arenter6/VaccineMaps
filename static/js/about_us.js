9963774f2ff6dbefe53ff520cbdfa637421bf1c2
= <dapang@ucsc.edu>
2021-06-04 11:04:48 -0700

    Oh ok

--- static/js/about_us.js
+++ static/js/about_us.js
@@ -1,6 +1,5 @@
 var david_quotes = [
   "Here for a long time not a good time.",
-  "David likes to go hiking, watching movies and programming.",
   "Pro tip: refresh the page to receive more free wisdom.",
   "David is a 2nd year CS student at UCSC. His favorite pre-covid activity was to program.",
   "Are you still refreshing the page?",
@@ -8,8 +7,6 @@ var david_quotes = [
   "Hey, stop reading this and go outside!",
   "Are you vaccinated yet?",
   "Made you look.",
-  "If you are reading this, chances are that you are not blind.",
-  "Is your birthday this year? If so, happy birthday!",
   "Fun fact: If you google \"241543903\" and go to images you'll get a surprise!",
 ];
 
